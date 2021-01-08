cbc72c037bde5594b40003cfefa048e400a18d57
Radicorp <unduhdrive@gmail.com>
2021-01-09 00:46:04 +0800

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,44 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playsound(number) {
+    final player = AudioCache();
+    player.play('note$number.wav');
+  }
+
+  Expanded buildKey({Color color, int note}) {
+    return Expanded(
+      child: FlatButton(
+          color: color,
+          onPressed: () {
+            playsound(note);
+          }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                buildKey(color: Colors.blue, note: 1),
+                buildKey(color: Colors.teal, note: 2),
+                buildKey(color: Colors.red, note: 3),
+                buildKey(color: Colors.orange, note: 4),
+                buildKey(color: Colors.purple, note: 5),
+                buildKey(color: Colors.yellow, note: 6),
+                buildKey(color: Colors.green, note: 7),
+              ],
+            ),
+          ),
         ),
       ),
     );
